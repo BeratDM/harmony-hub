175d50ef40dc39d2d8ec914bac8bf1e18e72a8f8
BeratDM <check_point2@hotmail.com>
2025-08-15 20:17:58 +0300

    first commit

---
+++ hub.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, abort
+import hmac
+import hashlib
+import subprocess
+import os
+
+app = Flask(__name__)
+
+NIKO_GITHUB_WEBHOOK_SECRET = os.getenv("NIKO_GITHUB_WEBHOOK_SECRET", "")
+MANUAL_SECRET = os.getenv("MANUAL_SECRET", "")
+
+
+@app.route("/niko-deploy", methods=["POST"])
+def deploy():
+    # Check if this is GitHub webhook
+    sig = request.headers.get("X-Hub-Signature-256")
+    if sig:
+        body = request.data
+        expected_sig = (
+            "sha256="
+            + hmac.new(
+                NIKO_GITHUB_WEBHOOK_SECRET.encode(), body, hashlib.sha256
+            ).hexdigest()
+        )
+        if not hmac.compare_digest(sig, expected_sig):
+            abort(403, description="Invalid signature or token")
+    else:
+        # Manual trigger check
+        token = request.headers.get("X-Manual-Token")
+        if token != MANUAL_SECRET:
+            abort(403, description="Invalid signature or token")
+
+    print("deploying niko-discord-bot")
+    # Run deployment script
+    subprocess.Popen(["/home/bx/bxProjects/niko-discord-bot/util/deploy.sh"])
+    return "Deployment started\n", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9999)
